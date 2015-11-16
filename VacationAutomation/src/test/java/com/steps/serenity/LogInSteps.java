88252a4f62a8440f88722f0eda57731dbdf50111
Bernadett Baja <bernadett.baja@gmail.com>
2015-11-16 15:02:40 +0200

    initial project

---
+++ VacationAutomation/src/test/java/com/steps/serenity/LogInSteps.java
@@ -0,0 +1,18 @@
+package com.steps.serenity;
+
+import com.pages.LogInPage;
+
+import net.thucydides.core.annotations.Step;
+import net.thucydides.core.steps.ScenarioSteps;
+
+public class LogInSteps extends ScenarioSteps {
+
+    LogInPage logInPage;
+
+    @Step
+    public void login(String user, String pass) {
+    	logInPage.enter_username(user);
+    	logInPage.enter_password(pass);
+    	logInPage.click_login();
+    }
+}
