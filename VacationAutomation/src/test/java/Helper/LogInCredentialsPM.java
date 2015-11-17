1abc0686cdcd83771e538e12bbcca8f41f01a4d9
tudor.butiri@evozon.com <tudorbutiri@EVO010.evointern>
2015-11-17 12:24:21 +0200

    Package Helper created - login Credentials.

---
+++ VacationAutomation/src/test/java/Helper/LogInCredentialsPM.java
@@ -0,0 +1,14 @@
+package Helper;
+
+BaseUrl("http://172.22.4.88:9090")
+public class LogInCredentialsPM 
+{
+	private final static String userDM = ;
+	private final static String passDM = ;
+	
+	private final static String userPM = "tudor.butiri1";
+	private final static String passPM = "evoportal";
+	
+	private final static String userDEV = ;
+	private final static String passDEV = ;
+}
