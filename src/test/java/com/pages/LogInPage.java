f7a57f90aa672c8428d2c8bfe7c72224dcc08ecc
Bernadett Baja <bernadett.baja@gmail.com>
2015-11-20 11:49:31 +0200

    new constante

--- src/test/java/com/pages/LogInPage.java
+++ src/test/java/com/pages/LogInPage.java
@@ -1,11 +1,12 @@
 package com.pages;
 
+import Helper.constante;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
 import net.thucydides.core.pages.PageObject;
 
-@DefaultUrl("http://172.22.4.88:9090/vacation")
+@DefaultUrl(constante.BASEURL + "/vacation")
 public class LogInPage extends PageObject {
 
 	@FindBy(css = "input[id='_58_login']")
