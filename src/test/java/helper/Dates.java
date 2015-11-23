2903a5bc8a3d047774e449c65db1c48b6e53bb1f
Bernadett Baja <bernadett.baja@gmail.com>
2015-11-23 10:29:38 +0200

    update dates

--- src/test/java/helper/Dates.java
+++ src/test/java/helper/Dates.java
@@ -61,6 +61,13 @@ public class Dates
 	//Bernadette
 	
 	
+	public final static int pickstartday=6;
+	public final static String pickstartmonth = "Mar";
+	public final static int pickstartyear= 2018;
+	
+	public final static int pickendday=7;
+	public final static String pickendmonth = "Mar";
+	public final static int pickendyear= 2018;
 	
 	
 	
